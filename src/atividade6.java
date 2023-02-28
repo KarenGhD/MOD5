2da181050b29b52dcddfa54bb5efd60c42b2dde0
Karen <karen.sn.amb@gmail.com>
2023-02-28 11:31:09 -0300

    criando listas e percorrendo

--- src/atividade6.java
+++ src/atividade6.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class atividade6 {
+import java.util.ArrayList;
+import java.util.List;
+
+public class atividade6 {
+    public static void main (String[] args){
+        List<String> colecao = new ArrayList<>();
+        colecao.add("Barco");
+        colecao.add("Navio");
+        colecao.add("Lancha");
+
+        for( int  i=0; i<colecao.size() ;i++){
+            System.out.println(colecao.get(i));
+        }
+
+        colecao.remove(1);
+
+        for( int  i=0; i<colecao.size() ;i++){
+            System.out.println(colecao.get(i));
+        }
+
+
+
+
+    }
 }
