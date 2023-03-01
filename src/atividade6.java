d9d57c85862f5840c8c517b21d1c2ebaae9f3c0c
Karen <karen.sn.amb@gmail.com>
2023-03-01 11:05:10 -0300

    criando matriz de calendario

--- src/atividade6.java
+++ src/atividade6.java
@@ -10,11 +10,49 @@ public class atividade6 {
        numeros[1]=200;
        numeros[2]=300;
 
+        System.out.println("valores iniciais:");
+
        for( int i=0; i< numeros.length;i++){
            System.out.println(numeros[i]);
       }
-    }
+        System.out.println("Valores invertidos:");
 
+       int [] numerosReverso = new int[numeros.length];
+       int posicaoMaxima =numeros.length -1;
+
+       for( int i=posicaoMaxima;i>=0;i--){
+           numerosReverso[posicaoMaxima-i]=numeros[i];
+       }
+       for (int i=0; i< numerosReverso.length;i++){
+           System.out.println(numerosReverso[i]);
+
+       }
+
+        char[] letras = new char[5];
+        letras[0]='k';
+        letras[1]='a';
+        letras[2]='r';
+        letras[3]='e';
+        letras[4]='n';
+
+        System.out.print("nome formado:");
+
+        for( int a=0; a<letras.length ;a++){
+            System.out.print(letras[a]);
+        }
+        char[] nomeInvertido = new char[letras.length];
+        int posicaoMax= letras.length -1;
+
+        for( int a=posicaoMax; a>=0;a--) {
+            nomeInvertido[posicaoMax - a] = letras[a];
+        }
+        System.out.println("");
+        System.out.print("Nome invertido:");
+        for(int a=0;a<nomeInvertido.length;a++){
+            System.out.print(nomeInvertido[a]);
+        }
+
+        }
 
 
 
