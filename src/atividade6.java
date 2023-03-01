65f7a3b66a0bf943bb84fb6c505c6fa4865b35ad
Karen <karen.sn.amb@gmail.com>
2023-03-01 09:58:47 -0300

    criando array

--- src/atividade6.java
+++ src/atividade6.java
@@ -1,25 +1,22 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class atividade6 {
     public static void main (String[] args){
-        List<String> colecao = new ArrayList<>();
-        colecao.add("Barco");
-        colecao.add("Navio");
-        colecao.add("Lancha");
+       int[] numeros = new int[3];
+       numeros[0]=100;
+       numeros[1]=200;
+       numeros[2]=300;
 
-        for( int  i=0; i<colecao.size() ;i++){
-            System.out.println(colecao.get(i));
-        }
-
-        colecao.remove(1);
-
-        for( int  i=0; i<colecao.size() ;i++){
-            System.out.println(colecao.get(i));
-        }
-
-
-
-
+       for( int i=0; i< numeros.length;i++){
+           System.out.println(numeros[i]);
+      }
     }
-}
+
+
+
+
+   }
+
